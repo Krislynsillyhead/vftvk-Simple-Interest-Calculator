3dbf26c40302dcd228064b198a68918526ca31ce
Krislynsillyhead <runteus@gmail.com>
2022-04-12 09:52:31 -0700

    changes made on Visual Studio Code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coding-project-template
+This is my final project for Introduction to Web Design on Coursera by IBM
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+var principal = document.getElementById("amount").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+//defined variables
+if (principal <= 0 || isNaN(principal))
+    {
+        alert("Please enter a positive number.");
+        document.getElementById("amount").focus();
+        return
+    }
+//added an alert for a positive number. 
+var interest = principal*years*rate/100;
+var year = new Date().getFullYear()+parseInt(years);
+document.getElementById("result").innerHTML="If you deposit <b>$"+principal+"</b>\<br\>at an interest rate of <b>"+rate+"%</b>\<br\>You will receive an amount of <b>$"+interest+"</b>,\<br\>in the year <b>"+year+"</b>\<br\>"
 }
-        
+//Maths
+function updateRate()
+{
+var rateval=document.getElementById("rate").value;
+document.getElementById("rate_val").innerText=rateval+"%";
+}
+//Check out https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps to learn lots of great javascript!
